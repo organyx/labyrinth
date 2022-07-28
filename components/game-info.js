b40b43aad0c854863ed6a8071fd4e5fe59803fed
AleksBeliun <aleksandr@novoresume.com>
2022-07-28 17:51:54 +0300

    feat: :sparkles: Hide game info if mazeId is not set

--- components/game-info.js
+++ components/game-info.js
@@ -9,13 +9,17 @@ const GameInfo = () => {
   const { gameState, playerName, error, mazeId } = useContext(LabyrinthContext);
   return (
     <>
-      <Title order={3}>The Game ID: {mazeId} </Title>
-      <Title order={3}>Player: {playerName}</Title>
-      {error && <Title order={3}>Error: {error}</Title>}
-      <Title order={3} className={styles['game-status']}>
-        Game Status: <span className={styles['game-status--highlight']}>{gameState.state}</span>
-      </Title>
-      <Title order={4}>{gameState['state-result']}</Title>
+      {mazeId && (
+        <>
+          <Title order={3}>The Game ID: {mazeId} </Title>
+          <Title order={3}>Player: {playerName}</Title>
+          {error && <Title order={3}>Error: {error}</Title>}
+          <Title order={3} className={styles['game-status']}>
+            Game Status: <span className={styles['game-status--highlight']}>{gameState.state}</span>
+          </Title>
+          <Title order={4}>{gameState['state-result']}</Title>
+        </>
+      )}
     </>
   );
 };
